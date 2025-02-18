1c0a66737d785c8d66d286f9862f596834b5d468
Andrew Winter <winterad@gib.cc>
2025-02-18 00:59:24 -0500

    I have the Curl working in this .net application using the sample audio file provided from the trial...

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Text;
+﻿using Deepgram;
+using System.Text.Json;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Deepgram.Models.PreRecorded.v1;
+using System.Net.Http;
+using System.Text;
 
 namespace DeepGram_Sample_Project
 {
@@ -16,6 +12,12 @@ namespace DeepGram_Sample_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+        internal static string apiKey = "{APIKEY_HERE}";
+        internal static string apiUrl = "https://api.deepgram.com/v1/listen?smart_format=true&model=nova-2&language=en-US";
+        internal static string jsonPayload = "{\"url\":\"https://static.deepgram.com/examples/Bueller-Life-moves-pretty-fast.wav\"}";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -23,7 +25,44 @@ namespace DeepGram_Sample_Project
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // Initialize Library with default logging ("Info" level)
+            Library.Initialize();
 
+            // The API key we created in step 3
+            //var deepgramClient = new ListenRESTClient(secret2);
+
+            // Hosted sample file
+            //var audioUrl = "https://static.deepgram.com/examples/Bueller-Life-moves-pretty-fast.wav";
+            try
+            {
+                string result = Task.Run(async () => await MakeApiCall()).GetAwaiter().GetResult();
+                tbb_response.Text = result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+
+        internal static async Task<string> MakeApiCall()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", $"Token {apiKey}");
+                var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    return $"Error: {response.StatusCode}";
+                }
+            }
         }
     }
 }
